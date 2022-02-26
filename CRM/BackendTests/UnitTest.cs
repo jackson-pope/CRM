7f994b359d042637adfac71b3a2cf7824183c27f
Jackson Pope <100478818+jackson-pope@users.noreply.github.com>
2022-02-26 20:56:01 +0000

    Initial solution
    Created initial solution and projects

---
+++ CRM/BackendTests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackendTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
