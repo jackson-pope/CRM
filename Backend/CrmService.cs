c35547b8219c4c6c5d858bbda2e3d01d2c516bd8
Jackson Pope <jack@eurydicegames.co.uk>
2022-02-27 16:54:33 +0000

    Added a context
    Also added a very basic unit test to ensure they are wired up correctly.

---
+++ Backend/CrmService.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Models;
+
+namespace Backend
+{
+    public class CrmService
+    {
+        private CrmContext _context;
+
+        public CrmService(CrmContext context)
+        {
+            _context = context;
+        }
+
+        //public Blog AddBlog(string name, string url)
+        //{
+        //    var blog = _context.Blogs.Add(new Blog { Name = name, Url = url });
+        //    _context.SaveChanges();
+
+        //    return blog;
+        //}
+
+        public List<Customer> GetAllCustomers()
+        {
+            return _context.Customers.OrderBy(c => c.Name).ToList();
+        }
+    }
+}
---
+++ BackendTests/CrmServiceTests.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+using Backend.Models;
+using Backend;
+
+namespace BackendTests
+{
+    namespace TestingDemo
+    {
+        [TestFixture]
+        public class CrmServiceTests
+        {
+            [Test]
+            public void GetAllCustomers_sortsByName()
+            {
+                var data = new List<Customer>
+                            {
+                                new Customer { Name = "Bob" },
+                                new Customer { Name = "Archie" },
+                                new Customer { Name = "Charlie" },
+                            }.AsQueryable();
+
+                var mockSet = new Mock<DbSet<Customer>>();
+                mockSet.As<IQueryable<Customer>>().Setup(m => m.Provider).Returns(data.Provider);
+                mockSet.As<IQueryable<Customer>>().Setup(m => m.Expression).Returns(data.Expression);
+                mockSet.As<IQueryable<Customer>>().Setup(m => m.ElementType).Returns(data.ElementType);
+                mockSet.As<IQueryable<Customer>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+                var mockContext = new Mock<CrmContext>();
+                mockContext.Setup(m => m.Customers).Returns(mockSet.Object);
+
+                var service = new CrmService(mockContext.Object);
+
+                var customers = service.GetAllCustomers();
+
+                Assert.AreEqual(3, customers.Count);
+                Assert.AreEqual("Archie",  customers[0].Name);
+                Assert.AreEqual("Bob",     customers[1].Name);
+                Assert.AreEqual("Charlie", customers[2].Name);
+            }
+        }
+    }
+}
