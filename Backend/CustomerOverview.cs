bf66f6bd829f359d97288ffb83237eb7476c14f5
Jackson Pope <jack@eurydicegames.co.uk>
2022-02-27 17:25:50 +0000

    Added an overview for the main list
    And a coupole of tests for creation and the Customer LTV

---
+++ Backend/CustomerOverview.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Models;
+
+namespace Backend
+{
+    public class CustomerOverview
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+        public string EmailAddress { get; private set; }
+        public decimal LTV { get; private set; }
+
+        public CustomerOverview(Customer customer)
+        {
+            Id = customer.Id;
+            Name = customer.Name;
+            Country = customer.Country;
+            EmailAddress = customer.EmailAddress;
+            LTV = customer.Invoices.Sum(i => i.InvoiceTotal);
+        }
+    }
+}
---
+++ BackendTests/CustomerOverviewTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Backend.Models;
+using Backend;
+
+namespace BackendTests
+{
+    [TestFixture]
+    public class CustomerOverviewTests
+    {
+        [Test]
+        public void Constructor_PopulatesWithFullCustomerHighlights()
+        {
+            // Arrange
+            var customer = new Customer { Name = "Archie", Country = "GB", EmailAddress = "archie@example.com" };
+
+            //Act
+            var overview = new CustomerOverview(customer);
+
+            //Assert
+            Assert.That(overview.Name,         Is.EqualTo(customer.Name));
+            Assert.That(overview.Country,      Is.EqualTo(customer.Country));
+            Assert.That(overview.EmailAddress, Is.EqualTo(customer.EmailAddress));
+        }
+
+        [Test]
+        public void LTV_IsEqualToSumOfInvoices()
+        {
+            // Arrange
+            var invoice1 = new Invoice { InvoiceTotal = 120.00M };
+            var invoice2 = new Invoice { InvoiceTotal = 54.50M };
+            var invoices = new List<Invoice> { invoice1, invoice2 };
+            var customer = new Customer { Name = "Archie", Country = "GB", EmailAddress = "archie@example.com", Invoices = invoices };
+
+            //Act
+            var overview = new CustomerOverview(customer);
+
+            //Assert
+            Assert.That(overview.LTV, Is.EqualTo(invoice1.InvoiceTotal + invoice2.InvoiceTotal));
+        }
+    }
+}
